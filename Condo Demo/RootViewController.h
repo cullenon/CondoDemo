1743a5d7ae89250bd1183cfdb3aef6894f85b11f
cullen <cullen.oneill@gmail.com>
2012-10-05 19:46:43 -0400

    Initial Commit

---
+++ Condo Demo/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Condo Demo
+//
+//  Created by Cullen O'Neill on 10/5/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Condo Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Condo Demo
+//
+//  Created by Cullen O'Neill on 10/5/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
